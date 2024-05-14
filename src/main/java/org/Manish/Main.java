9f5cf4655b1aabf559f4fdef599301c9e760ad4b
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-14 09:57:51 +0530

    NullPointer exception example

--- src/main/java/org/Manish/Main.java
+++ src/main/java/org/Manish/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         int a = 10 ;
-        int b =0;
+        int b =10;
         int c=0;
+        String s = null;
         try{
-             c = a/b;
+             c = a/b; //Division by zero
+             s= s.toLowerCase();//Null value
         }catch (ArithmeticException e ){
+            System.err.println("Exception :"+e.getMessage());
+        }catch (NullPointerException e){
             System.err.println("Exception :"+e.getMessage());
         }
         System.out.println(c);
