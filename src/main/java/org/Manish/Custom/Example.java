cb3e880047766ab0f537c965c73caf4518094a75
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-14 10:32:55 +0530

    created custom Exception and used in Example

---
+++ src/main/java/org/Manish/Custom/Example.java
@@ -0,0 +1,30 @@
+package org.Manish.Custom;
+
+
+public class Example {
+    public static void main(String[] args) {
+        try {
+
+           if(above18(2))
+           {
+               System.out.println("Right age");
+
+           }else {
+               throw new CustomEx("Below the right age",new Throwable("Not right age"));
+           }
+        } catch (CustomEx e) {
+            // Catch and handle the custom exception
+            System.out.println("Custom exception caught: " + e.getMessage());
+            System.out.println("Custom exception caught: " + e.getCause());
+
+        }
+    }
+
+    public static boolean above18(int age)
+    {
+        if(age>=18)
+            return true;
+        else
+            return false;
+    }
+}
