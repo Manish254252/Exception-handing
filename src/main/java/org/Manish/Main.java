d8357aedc297218270da45f1fa25f5b962e597ce
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-14 10:36:40 +0530

    optimized some code

--- src/main/java/org/Manish/Main.java
+++ src/main/java/org/Manish/Main.java
@@ -3,32 +3,31 @@ package org.Manish;
 public class Main {
     public static void main(String[] args) {
 
-//        int a = 10 ;
-//        int b =10;
-//        int c=0;
-//        String s = null;
-//        try{
-//             c = a/b; //Division by zero
-//             s= s.toLowerCase();//Null value
-//        }catch (ArithmeticException e ){
-//            System.err.println("Exception :"+e.getMessage());
-//        }catch (NullPointerException e){
-//            System.err.println("Exception :"+e.getMessage());
-//        }
-//        System.out.println(c);
+        int a = 10 ;
+        int b =10;
+        int c=0;
+        String s = "null";
+        try{
+             c = a/b; //Division by zero
+             s= s.toLowerCase();//Null value
+            System.out.println(s);
+        }catch (ArithmeticException | NullPointerException e ){
+            System.err.println("Exception :"+e.getMessage());
+        }
+        System.out.println(c);
         try
         {
             divide(5,10);
             divide(5,0);
         }catch (ArithmeticException e )
         {
-            System.err.println(e.getCause());
+//            System.err.println(e.getCause());
             System.err.println(e.getMessage());
         }
 
     }
     public static void divide(int x , int y ) throws ArithmeticException{
-        int q=0;
+        int q;
         q=x/y;
         System.out.println(q);
     }
