bb96bd858e6e3cd7bd4636b877169b0cda05d4d4
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-14 09:54:23 +0530

    Arithmetic exception example

--- src/main/java/org/Manish/Main.java
+++ src/main/java/org/Manish/Main.java
@@ -2,6 +2,16 @@ package org.Manish;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 10 ;
+        int b =0;
+        int c=0;
+        try{
+             c = a/b;
+        }catch (ArithmeticException e ){
+            System.err.println("Exception :"+e.getMessage());
+        }
+        System.out.println(c);
+
     }
 }
